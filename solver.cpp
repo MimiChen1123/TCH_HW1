25948a3da05f947a1e22e26ddc6ad0dfe51796ff
MimiChen1123 <mimi911123@gmail.com>
2025-10-02 16:00:38 +0800

    except SH-3

--- solver.cpp
+++ solver.cpp
@@ -3,19 +3,11 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
 #include <string>
 #include <time.h>
+#include <typeinfo>
 using namespace std;
-
-/*
-// 1. 怎麼執行？我需要處理 input 嗎？
-2. do_move 到底是做什麼？他回傳的是代表是什麼意思？
-3. distance<File> 是什麼意思？
-4. 把 Red 吃掉這個行為是我需要去實作的，還是呼叫哪一個 function 即可？
-5. 我 pos 是什麼時候更新的？
-6. Invalid moves 是什麼意思？我自己跑看起來很合理呀？
-*/
 
 
 /*
@@ -31,23 +23,15 @@ using namespace std;
  * Good luck!
  */
 
-struct Status {
-    Position pos;
-    int g;
-    int h;
-    vector<Move> path;
-    int f() const {
-        return g + h;
-    }
 
-    bool operator>(const Status &other) const {
-        return f() > other.f();
+struct cmp {
+    bool operator()(const pair<int, string> a, const pair<int, string> b) const {
+        return a.first > b.first;
     }
 };
 
 int heuristic(Position &pos){
     Board red_pieces = pos.pieces(Red);
-    // return __builtin_popcount(red_pieces);
     return BoardView(red_pieces).to_vector().size();
 }
 
@@ -55,60 +39,68 @@ void resolve(Position &pos)
 {
     struct timespec start_time, end_time;
     clock_gettime(CLOCK_REALTIME, &start_time);
-    /* You can use these aliases */
-    // info << "Output (only) your answers here!\n"; // ====> stdout <====
-    // debug << "Print debug info here!\n";          // stderr
-    // error << "Print errors here?\n";              // also stderr
 
-    // info << pos;
+    info << pos;
 
     /* A random number generator is available globally */
     int random_num_below_42 = rng(42);
 
-    Status start = {pos, 0, heuristic(pos), {}};
-    priority_queue<Status, vector<Status>, greater<Status>> pq;
-    pq.push(start);
+    string start = pos.toFEN();
+    priority_queue<pair<int, string>, vector<pair<int, string>>, cmp> pq;
+    pq.push({heuristic(pos), start});
 
-    unordered_set<string> visited;
-    visited.insert(pos.toFEN());
+    unordered_map<string, int> visited; // key: fen, value: moves(g)
+    unordered_map<string, string> path; // key: cur.fen , value: prev.fen
+    unordered_map<string, Move> moves; //key: fen, value: move
+
+    visited[start] = 0;
+    path[start] = start;
 
     while(!pq.empty()){
-        Status cur = pq.top();
+        pair<int, string> cur = pq.top();
+        string cur_key = cur.second;
         pq.pop();
-
-        if(cur.pos.winner() == Black){
+        
+        Position cur_pos(cur_key);
+        
+        if(cur_pos.winner() == Black){
             clock_gettime(CLOCK_REALTIME, &end_time);
             double wall_clock_in_seconds =(double)((end_time.tv_sec + end_time.tv_nsec * (1e-9))
                                              - (double)(start_time.tv_sec + start_time.tv_nsec * (1e-9)));
     
             info << wall_clock_in_seconds << "\n";
-            info << cur.g << "\n";
-            int count = 1;
-            for(Move mv: cur.path){
+            info << visited[cur_key] << "\n";
+
+            string tmp = cur_key;
+            vector<Move> ans;
+            while(tmp != path[tmp]){
+                ans.push_back(moves[tmp]);
+                tmp = path[tmp];
+            }
+            reverse(ans.begin(), ans.end());
+            for (auto mv : ans) {
                 info << mv;
-                count++;
             }
             return;
         }
 
-        MoveList mvs(cur.pos);
+        MoveList mvs(cur_pos);
 
         for(Move mv: mvs){
-            Position copy_pos(cur.pos);
+            Position copy_pos(cur_pos);
             if(!copy_pos.do_move(mv)){
                 continue;
             }
-
-            string key = copy_pos.toFEN();
-            if(visited.find(key) != visited.end()){
-                continue;
-            }
             
-            cur.path.push_back(mv);
-            Status next = {copy_pos, cur.g + 1, heuristic(copy_pos), cur.path};
-            pq.push(move(next));
-            visited.insert(key);
-            cur.path.pop_back();
+            string key = copy_pos.toFEN();
+            int cost = visited[cur_key] + 1;
+            if(visited.find(key) == visited.end() || cost < visited[key]){
+                visited[key] = cost;
+                path[key] = cur_key;
+                moves[key] = mv;
+
+                pq.push({cost + heuristic(copy_pos), key});
+            }
         }
         
     }
